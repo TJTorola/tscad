391142bf2a4db1576d70264f1f57f10674e0fc88
Tyler Torola <Tyler@TJT.Codes>
2025-10-21 19:37:34 -0400

    Abstract out example geometry

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import { cube } from '@jscad/modeling/src/primitives';
 import Renderer from './Renderer';
 import { translate } from '@jscad/modeling/src/operations/transforms';
 import { useMemo } from 'react';
+import type { Geom3 } from '@jscad/modeling/src/geometries/types';
 
-const App = () => {
-  const geometries = useMemo(() => {
-    var ret = [];
-    var size = 10;
-    for (var i = 0; i < size; i++) {
-      for (var j = 0; j < size; j++) {
-        for (var k = 0; k < size; k++) {
-          ret.push(translate([i * 2, j * 2, k * 2], cube({ size: 1 })));
-        }
+export const cubeOfCubes = (size: number) => {
+  const cubes: Array<Geom3> = [];
+  for (var i = 0; i < size; i++) {
+    for (var j = 0; j < size; j++) {
+      for (var k = 0; k < size; k++) {
+        cubes.push(translate([i * 20, j * 20, k * 20], cube({ size: 10 })));
       }
     }
-    return ret;
-  }, []);
+  }
+  return cubes;
+};
+
+const App = () => {
+  const geometries = useMemo(() => cubeOfCubes(10), []);
   return <Renderer geometries={geometries} />;
 };
 
